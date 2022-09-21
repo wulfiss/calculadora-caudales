cb8bcd8a2e25247f921c3a0661e953fefdd346a3
wulfiss <96155980+wulfiss@users.noreply.github.com>
2022-09-21 11:08:39 -0300

    lots of changes

--- meterscubic.js
+++ meterscubic.js
@@ -1,15 +1,38 @@
-let arr = [4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500];
+const arrZero =[
+    {velNoria: 4000, caudal: 800},
+    {velNoria: 4500, caudal: 900},
+    {velNoria: 5000, caudal: 1000},
+    {velNoria: 5500, caudal: 1100},
+    {velNoria: 6000, caudal: 1200},
+    {velNoria: 6500, caudal: 1300},
+    {velNoria: 7000, caudal: 1400},
+    {velNoria: 7500, caudal: 1500},
+    {velNoria: 8000, caudal: 1600},
+    {velNoria: 8500, caudal: 1700}
+];
 
-console.log(arr);
-
-let noriaV = 4600;
-
-function filterTest(item){
-    if(item > noriaV){
-        return true;
-    } 
+function findObject(select, noria){
+    if(select == 'esDuMC'){
+        arr = arrZero;
+    }
+    return arr.find(item => item.velNoria > noria);
 }
 
-let arrTwo = arr.find(filterTest);
+let $calcularMC = document.querySelector('#calcularMC');
 
-console.log(arrTwo);
+$calcularMC.addEventListener('click', (e) => {
+    let noriaV = document.querySelector('#noriaMC').value;
+    let caudalL = document.querySelector('#caudalMC').value;
+    let divResult = document.querySelector('#resultMC');
+    let $select = document.querySelector('#CTypeMC').value;
+
+    let arrObjet = findObject($select, noriaV);
+    
+    if(caudalL <= arrObjet.caudal){
+        divResult.textContent = 'Caudal insuficiente';
+    } else if(caudalL > arrObjet.caudal){
+        divResult.textContent = 'Cadual aceptable';
+    }
+    
+})
+
