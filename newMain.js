fa14d935013f16318f5c8e42594c42331dad54cf
wulfiss <96155980+wulfiss@users.noreply.github.com>
2022-09-23 12:09:39 -0300

    Add some module

--- newMain.js
+++ newMain.js
@@ -174,3 +174,120 @@
     };
 
 })();
+/*
+(function calculator(){
+
+    const subMinutes = (values) =>{
+        const validate = time =>{
+            if(time > 59 || time < 0){
+                throw new Error(
+                    "Hours, minutes and seconds values have to be between 0 and 59."
+                );
+            }
+            return time;
+        };
+    
+        const seconds = values
+            .map(e => validate(Number(e.split(":").reverse()[0])))
+            .reduce((a, b) => a - b);
+    
+        let minutes = values
+            .map(e => validate(Number(e.split(":").reverse()[1])))
+            .reduce((a, b) => a - b);
+    
+        let hours = values
+            .map(e =>
+            e.split(":").reverse()[2] ? Number(e.split(":").reverse[2]) : 0)
+            .reduce((a, b) => a - b);
+    
+        minutes *= 60;
+        hours *= 3600;
+    
+        let result = new Date((hours + minutes + seconds) * 1000)
+            .toISOString()
+            .substr(11, 8);
+    
+        return result.split(":").reverse()[2] === "00" ? result.slice(3) : result;
+    }
+    
+    const timeStringToFloat = (time) =>{
+        let hoursMinutes = time.split(':');
+        let hours = parseInt(hoursMinutes[0], 10);
+        let minutes = hoursMinutes[1] ? parseInt(hoursMinutes[1], 10) : 0;
+        return hours + minutes / 60;
+    }
+    
+    const input = () => {
+        let $Calcular = document.querySelector('#BCalcular');
+        let $DeNuevo = document.querySelector('#BDeNuevo');
+        let resultDiv = document.querySelector('#ResultDiv');
+        let formStart = document.querySelector('#formStart');
+        let timeOne = document.querySelector('#ITime').value;
+        let timeTwo = document.querySelector('#FTime').value;
+        let litrosOne = document.querySelector('#ICaudal').value;
+        let litrosTwo = document.querySelector('#FCaudal').value;
+        let vNoria = document.querySelector('#VNoria').value;
+        let $select = document.querySelector('#CType').value;
+    
+        let lPorAvesS = document.querySelector('#LPorAves');
+        let interTiempoS = document.querySelector('#interTiempo');
+        let lConsumidosS = document.querySelector('#LConsumidos');
+
+        return{
+            $Calcular, $DeNuevo, resultDiv, formStart, timeOne, timeTwo,
+            litrosOne, litrosTwo, vNoria, $select, lPorAvesS, interTiempoS,
+            lConsumidosS
+        }
+    }
+
+    $Calcular.addEventListener('click', (e) =>{
+
+        let variable = 1;
+        let unitT = 'L/aves';
+    
+        if ($select == 'esGarras' || $select == 'chG'){
+            variable = 0.07;
+            unitT = 'L/Kg de garras'
+        } else if($select == 'chHC'){
+            variable = 0.075;
+            unitT = 'L/Kg de Higado - Corazon';
+        } else if($select == 'chM'){
+            variable = 0.03;
+            unitT = 'L/Kg de Molleja';
+        } else if($select == 'chC'){
+            variable = 0.08;
+            unitT = 'L/Kg de cogotes'
+        }
+        
+        let litrosT = litrosTwo - litrosOne;
+        let restTime = subMinutes([timeTwo, timeOne]);
+        let restTimeFloat = timeStringToFloat(restTime);
+        let litresPerChicken = (((litrosTwo - litrosOne)/(vNoria * restTimeFloat * variable)).toFixed(2)) + " " + unitT;
+    
+        lPorAvesS.textContent = litresPerChicken;
+        interTiempoS.textContent = restTime + " Horas";
+        lConsumidosS.textContent = litrosT + " Litros";
+        
+        displayHidden(true);
+
+    });
+    
+    const displayHidden = (state) =>{
+        if(state == true){
+            input["resultDiv"].style.display = 'block';
+            input["formStart"].style.display = 'none';
+        }else if(state == false){
+            input["resultDiv"].style.display = 'none';
+            input["formStart"].style.display = 'block';
+        }
+    }
+
+    input["$DeNuevo"].addEventListener('click', (e) =>{
+
+        displayHidden(false);
+
+    });
+
+})();
+
+*/
