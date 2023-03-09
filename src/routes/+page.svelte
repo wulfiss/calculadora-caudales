4f70d1233cf99d5c02b63f7c080c550b519ed45c
Villalba Julian <96155980+wulfiss@users.noreply.github.com>
2023-03-08 22:50:30 -0300

    Initial commit new try

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script lang='ts'>
+    import Tab, { Label } from '@smui/tab';
+    import TabBar from '@smui/tab-bar';
+    import Fast from './Components/Fast.svelte';
+    import Full from './Components/Full.svelte';
+
+    let tabs=[
+    {id:0, a: 'Fast', name: 'Calculadora Rápida'},
+    {id:1, a: 'Full', name: 'Calculadora'},
+    ];
+
+    const actions =  [Fast, Full];
+
+    let action = actions[0];
+    let active = tabs[0];
+
+    function handleActionClicked(id:number):any {
+        action = actions[id];
+    }
+</script>
+
+<TabBar {tabs} let:tab bind:active>
+    <!-- Note: the `tab` property is required! -->
+    <Tab {tab} on:click={handleActionClicked(tab.id)}>
+      <Label>{tab.name}</Label>
+    </Tab>
+  </TabBar>
+
+
+  <svelte:component this={action}></svelte:component>
